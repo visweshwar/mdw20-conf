9c73ec178c56e0b87b934d909315c350f581c776
Visweshwar Ganesh <vganesh@paychex.om>
2020-03-08 20:21:40 -0400

    Initial Commit

---
+++ DemoHRApplication/src/main/java/com/paychex/mdw20/hrapplication/entity/Client.java
@@ -0,0 +1,57 @@
+/*
+ *
+ *  * # Copyright 2020 Paychex, Inc.
+ *  * # Licensed pursuant to the terms of the Apache License, Version 2.0 (the "License");
+ *  * # your use of the Work is subject to the terms and conditions of the License.
+ *  * # You may obtain a copy of the License at
+ *  * #
+ *  * # http://www.apache.org/licenses/LICENSE-2.0
+ *  * #
+ *  * # Disclaimer of Warranty. Unless required by applicable law or agreed to in writing, Licensor
+ *  * # provides the Work (and each Contributor provides its Contributions) on an "AS IS" BASIS,
+ *  * # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied, including,
+ *  * # without limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT,
+ *  * # MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. You are solely responsible
+ *  * # for determining the appropriateness of using or redistributing the Work and assume
+ *  * # any risks associated with your exercise of permissions under this License.
+ *
+ */
+
+package com.paychex.mdw20.hrapplication.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/*
+    Author: Visweshwar Ganesh 
+   created on 3/8/20 
+*/
+@Document(collection = "clients")
+public class Client {
+
+	private String clientId;
+
+	private String clientName;
+
+	public Client(String clientName) {
+		this.clientName = clientName;
+	}
+
+	public Client() {
+	}
+
+	public String getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+
+	public String getClientName() {
+		return clientName;
+	}
+
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+}
