c3c47143087d0076872477256bec797879e8e245
Visweshwar Ganesh <vganesh@paychex.om>
2020-03-15 07:59:15 -0400

    Add mongo CSFLE

--- application/src/test/java/com/paychex/mdw20/hrapplication/ApplicationTests.java
+++ application/src/test/java/com/paychex/mdw20/hrapplication/ApplicationTests.java
@@ -19,14 +19,13 @@
 
 package com.paychex.mdw20.hrapplication;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//	@Test
+	//	void contextLoads() {
+	//	}
 
 }
