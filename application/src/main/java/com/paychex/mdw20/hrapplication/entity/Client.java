f5d8598e7f4f8029d3ea18ba9946baeca1886316
Visweshwar Ganesh <vganesh@paychex.om>
2020-05-01 14:18:50 -0400

    changingID

--- application/src/main/java/com/paychex/mdw20/hrapplication/entity/Client.java
+++ application/src/main/java/com/paychex/mdw20/hrapplication/entity/Client.java
@@ -23,6 +23,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.HashMap;
 import java.util.Map;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
