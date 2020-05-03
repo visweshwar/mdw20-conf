1163fe79784c7313d10b68ca9982b441bf95d0e7
Visweshwar Ganesh <vganesh@paychex.om>
2020-05-03 19:54:59 -0400

    changing port number to 5006

--- application/docker/script.sh
+++ application/docker/script.sh
@@ -4,4 +4,4 @@ mkdir -p /keys # file.txt will come at the end of the script
 echo $MASTER_KEY >>/keys/master-keys.txt
 
 #supervisord -c "/etc/supervisor.conf"
-java -agentlib:jdwp=transport=dt_socket,address=5005,server=y,suspend=n -Xms256m -Xmx512m -XX:MaxMetaspaceSize=128m -jar /app.jar
+java -agentlib:jdwp=transport=dt_socket,address=5006,server=y,suspend=n -Xms256m -Xmx512m -XX:MaxMetaspaceSize=128m -jar /app.jar
