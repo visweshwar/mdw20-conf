fb9da943b291dba89935a85e4448dc0c60d1e51a
Visweshwar Ganesh <vganesh@paychex.om>
2020-05-06 10:06:00 -0400

    Fixing compilation issues

--- application/src/main/java/com/paychex/mdw20/hrapplication/graphql/ClientResolver.java
+++ application/src/main/java/com/paychex/mdw20/hrapplication/graphql/ClientResolver.java
@@ -3,20 +3,22 @@ package com.paychex.mdw20.hrapplication.graphql;
 import com.paychex.mdw20.hrapplication.entity.Client;
 import com.paychex.mdw20.hrapplication.entity.Employee;
 import com.paychex.mdw20.hrapplication.entity.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ClientResolver extends MongoEntityBase<Client> {
 
     @Autowired
-//    private EmployeeReactiveRepository employeeRepository;
+    //    private EmployeeReactiveRepository employeeRepository;
     private EmployeeRepository employeeRepository;
+    @Value(value = "${application.region}")
+    private String REGION;
 
     public List<Employee> getEmployees(Client client) {
-        return employeeRepository.getAllByClientId(client.getClientId())/*.collectList().block()*/;
+        return employeeRepository.getAllByClientIdAndRegion(client.getClientId(), REGION)/*.collectList().block()*/;
     }
 
 }
--- application/src/main/java/com/paychex/mdw20/hrapplication/graphql/Query.java
+++ application/src/main/java/com/paychex/mdw20/hrapplication/graphql/Query.java
@@ -3,10 +3,10 @@ package com.paychex.mdw20.hrapplication.graphql;
 import com.paychex.mdw20.hrapplication.entity.Client;
 import com.paychex.mdw20.hrapplication.entity.Employee;
 import com.paychex.mdw20.hrapplication.entity.repository.ClientReactiveRepository;
-import com.paychex.mdw20.hrapplication.entity.repository.EmployeeReactiveRepository;
 import com.paychex.mdw20.hrapplication.entity.repository.EmployeeRepository;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,8 +15,10 @@ public class Query implements GraphQLQueryResolver {
     @Autowired
     private ClientReactiveRepository clientRepository;
 
+    @Value(value = "${application.region}")
+    private String REGION;
     @Autowired
-//    private EmployeeReactiveRepository employeeRepository;
+    //    private EmployeeReactiveRepository employeeRepository;
     private EmployeeRepository employeeRepository;
 
     Client clientById(String id) {
@@ -24,6 +26,6 @@ public class Query implements GraphQLQueryResolver {
     }
 
     Employee employeeById(String id) {
-        return employeeRepository.findByEmployeeId(id)/*.block()*/;
+        return employeeRepository.findByEmployeeIdAndRegion(id, REGION)/*.block()*/;
     }
 }
