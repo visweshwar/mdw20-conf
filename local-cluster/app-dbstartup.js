e92f5c6d4026318e8b821c9d80939f190c1471e4
Visweshwar Ganesh <vganesh@paychex.om>
2020-05-06 09:50:12 -0400

    Adding Region to scripts

--- local-cluster/app-dbstartup.js
+++ local-cluster/app-dbstartup.js
@@ -10,122 +10,122 @@ sh.addShardTag("MDW20-eu", "EU-A");
 
 
 sh.enableSharding("client");
-db.clientDetails.ensureIndex({country: 1, active: 1, premium: 1, clientId: 1}, {name: "pre_ctry_cid_idx"});
+db.clientDetails.ensureIndex({region: 1, active: 1, premium: 1, clientId: 1}, {name: "pre_ctry_cid_idx"});
 //shard the client collection on premium or not
-sh.shardCollection('client.clientDetails', {country: 1, active: 1, premium: 1, clientId: 1});
+sh.shardCollection('client.clientDetails', {region: 1, active: 1, premium: 1, clientId: 1});
 
 sh.addTagRange(
     "client.clientDetails",
-    {"country": "USA", "active": true, "premium": false, "clientId": MinKey},
-    {"country": "USA", "active": true, "premium": false, "clientId": MaxKey},
+    {"region": "us", "active": true, "premium": false, "clientId": MinKey},
+    {"region": "us", "active": true, "premium": false, "clientId": MaxKey},
     "US-S"
 );
 
 sh.addTagRange(
     "client.clientDetails",
-    {"country": "USA", "active": true, "premium": true, "clientId": MinKey},
-    {"country": "USA", "active": true, "premium": true, "clientId": MaxKey},
+    {"region": "us", "active": true, "premium": true, "clientId": MinKey},
+    {"region": "us", "active": true, "premium": true, "clientId": MaxKey},
     "US-P"
 );
 
 sh.addTagRange(
     "client.clientDetails",
-    {"country": "USA", "active": false, "premium": true, "clientId": MinKey},
-    {"country": "USA", "active": false, "premium": true, "clientId": MaxKey},
+    {"region": "us", "active": false, "premium": true, "clientId": MinKey},
+    {"region": "us", "active": false, "premium": true, "clientId": MaxKey},
     "US-A"
 );
 
 sh.addTagRange(
     "client.clientDetails",
-    {"country": "USA", "active": false, "premium": false, "clientId": MinKey},
-    {"country": "USA", "active": false, "premium": false, "clientId": MaxKey},
+    {"region": "us", "active": false, "premium": false, "clientId": MinKey},
+    {"region": "us", "active": false, "premium": false, "clientId": MaxKey},
     "US-A"
 );
 
 sh.addTagRange(
     "client.clientDetails",
-    {"country": "UK", "active": true, "premium": false, "clientId": MinKey},
-    {"country": "UK", "active": true, "premium": false, "clientId": MaxKey},
+    {"region": "eu", "active": true, "premium": false, "clientId": MinKey},
+    {"region": "eu", "active": true, "premium": false, "clientId": MaxKey},
     "EU-S"
 );
 
 sh.addTagRange(
     "client.clientDetails",
-    {"country": "UK", "active": true, "premium": true, "clientId": MinKey},
-    {"country": "UK", "active": true, "premium": true, "clientId": MaxKey},
+    {"region": "eu", "active": true, "premium": true, "clientId": MinKey},
+    {"region": "eu", "active": true, "premium": true, "clientId": MaxKey},
     "EU-P"
 );
 
 sh.addTagRange(
     "client.clientDetails",
-    {"country": "UK", "active": false, "premium": true, "clientId": MinKey},
-    {"country": "UK", "active": false, "premium": true, "clientId": MaxKey},
+    {"region": "eu", "active": false, "premium": true, "clientId": MinKey},
+    {"region": "eu", "active": false, "premium": true, "clientId": MaxKey},
     "EU-A"
 );
 
 sh.addTagRange(
     "client.clientDetails",
-    {"country": "UK", "active": false, "premium": false, "clientId": MinKey},
-    {"country": "UK", "active": false, "premium": false, "clientId": MaxKey},
+    {"region": "eu", "active": false, "premium": false, "clientId": MinKey},
+    {"region": "eu", "active": false, "premium": false, "clientId": MaxKey},
     "EU-A"
 );
 
-db.employees.ensureIndex({country: 1, active: 1, premium: 1, employeeId: 1}, {name: "pre_ctry_eid_idx"});
+db.employees.ensureIndex({region: 1, active: 1, premium: 1, employeeId: 1}, {name: "pre_ctry_eid_idx"});
 //shard the client collection on premium or not
-sh.shardCollection('client.employees', {country: 1, active: 1, premium: 1, employeeId: 1});
+sh.shardCollection('client.employees', {region: 1, active: 1, premium: 1, employeeId: 1});
 
 sh.addTagRange(
     "client.employees",
-    {"country": "USA", "active": true, "premium": false, "employeeId": MinKey},
-    {"country": "USA", "active": true, "premium": false, "employeeId": MaxKey},
+    {"region": "us", "active": true, "premium": false, "employeeId": MinKey},
+    {"region": "us", "active": true, "premium": false, "employeeId": MaxKey},
     "US-S"
 );
 
 sh.addTagRange(
     "client.employees",
-    {"country": "USA", "active": true, "premium": true, "employeeId": MinKey},
-    {"country": "USA", "active": true, "premium": true, "employeeId": MaxKey},
+    {"region": "us", "active": true, "premium": true, "employeeId": MinKey},
+    {"region": "us", "active": true, "premium": true, "employeeId": MaxKey},
     "US-P"
 );
 
 
 sh.addTagRange(
     "client.employees",
-    {"country": "USA", "active": false, "premium": true, "employeeId": MinKey},
-    {"country": "USA", "active": false, "premium": true, "employeeId": MaxKey},
+    {"region": "us", "active": false, "premium": true, "employeeId": MinKey},
+    {"region": "us", "active": false, "premium": true, "employeeId": MaxKey},
     "US-A"
 );
 sh.addTagRange(
     "client.employees",
-    {"country": "USA", "active": false, "premium": false, "employeeId": MinKey},
-    {"country": "USA", "active": false, "premium": false, "employeeId": MaxKey},
+    {"region": "us", "active": false, "premium": false, "employeeId": MinKey},
+    {"region": "us", "active": false, "premium": false, "employeeId": MaxKey},
     "US-A"
 );
 
 sh.addTagRange(
     "client.employees",
-    {"country": "UK", "active": true, "premium": false, "employeeId": MinKey},
-    {"country": "UK", "active": true, "premium": false, "employeeId": MaxKey},
+    {"region": "eu", "active": true, "premium": false, "employeeId": MinKey},
+    {"region": "eu", "active": true, "premium": false, "employeeId": MaxKey},
     "EU-S"
 );
 
 sh.addTagRange(
     "client.employees",
-    {"country": "UK", "active": true, "premium": true, "employeeId": MinKey},
-    {"country": "UK", "active": true, "premium": true, "employeeId": MaxKey},
+    {"region": "eu", "active": true, "premium": true, "employeeId": MinKey},
+    {"region": "eu", "active": true, "premium": true, "employeeId": MaxKey},
     "EU-P"
 );
 
 sh.addTagRange(
     "client.employees",
-    {"country": "UK", "active": false, "premium": true, "employeeId": MinKey},
-    {"country": "UK", "active": false, "premium": true, "employeeId": MaxKey},
+    {"region": "eu", "active": false, "premium": true, "employeeId": MinKey},
+    {"region": "eu", "active": false, "premium": true, "employeeId": MaxKey},
     "EU-A"
 );
 
 sh.addTagRange(
     "client.employees",
-    {"country": "UK", "active": false, "premium": false, "employeeId": MinKey},
-    {"country": "UK", "active": false, "premium": false, "employeeId": MaxKey},
+    {"region": "eu", "active": false, "premium": false, "employeeId": MinKey},
+    {"region": "eu", "active": false, "premium": false, "employeeId": MaxKey},
     "EU-A"
 );
